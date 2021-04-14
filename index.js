44b0963b0d9384a1e39872c811c03fb1262ecd09
Victoria <vep002@gmail.com>
2021-04-14 15:39:07 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+const dodger= document.getElementById('dodger');
+dodger.style.backgroundColor = "#FF69B4"
+dodger.style.bottom = "100px"
+dodger.style.left = "100px"
+
+//document.addEventListener('keydown', function(event){
+//    console.log(event)
+//})
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  } 
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left < 400) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+document.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowLeft") {
+    moveDodgerLeft()}
+    else if (event.key ==="ArrowRight") {
+        moveDodgerRight()
+    }
+  });
+ 
